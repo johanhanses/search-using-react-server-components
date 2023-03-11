f4f34f65611e8ae3ad5d9dae8b7cf24b2284c9f4
Johan Hanses <johanhanses@gmail.com>
2023-03-11 15:04:34 +0100

    Refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,47 @@
 import data from './data.json'
-import ResultTable from './result-table'
 
 export default async function Home({ searchParams }: { searchParams: { search: string } }) {
   const { data: tableData } = data
 
+  const searchString = searchParams.search
+
+  const filteredData = tableData.filter((data) => data.name.includes(searchString))
+
   return (
     <main className="p-12">
-      <ResultTable
-        tableData={tableData}
-        searchParams={searchParams}
-      />
+      <section className="mt-12">
+        <div className="relative overflow-x-auto min-w-[32rem]">
+          <table className="w-full text-sm text-left text-gray-50">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+              <tr>
+                <th
+                  scope="col"
+                  className="px-6 py-3"
+                >
+                  Id
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3"
+                >
+                  Name
+                </th>
+              </tr>
+            </thead>
+            <tbody className="text-gray-700">
+              {filteredData.map((data) => (
+                <tr
+                  key={data.id}
+                  className="bg-white border-b"
+                >
+                  <td className="px-6 py-4">{data.id}</td>
+                  <td className="px-6 py-4">{data.name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </section>
     </main>
   )
 }
